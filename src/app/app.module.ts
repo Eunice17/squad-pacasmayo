a35e52acae440336cc9a0b74e16887796a7d6472
Rolando <euniceancajima@gmail.com>
2021-07-18 07:12:25 -0500

    maqueta vista driver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,7 @@ import { NewRequirementComponent } from './view/main-client/components/new-requi
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -35,7 +34,8 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
