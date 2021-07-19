6a5092a66819810356aebdce180f85176d603be1
Yessenia <vela.kathy@gmail.com>
2021-07-19 17:22:31 -0500

    form group para el componente new requirement

--- src/app/view/main-client/components/new-requirement/new-requirement.component.ts
+++ src/app/view/main-client/components/new-requirement/new-requirement.component.ts
@@ -3,17 +3,6 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { OriginI, DestinationI } from '../../../../models/dinoex';
 import { OriginService } from '../../../../services/origin.service';
 
-
-interface Food {
-  value: string;
-  viewValue: string;
-}
-
-interface Car {
-  value: string;
-  viewValue: string;
-}
-
 @Component({
   selector: 'app-new-requirement',
   templateUrl: './new-requirement.component.html',
@@ -21,50 +10,32 @@ interface Car {
 })
 export class NewRequirementComponent implements OnInit {
 
-  form: FormGroup;
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
-  cars: Car[] = [
-    {value: 'volvo', viewValue: 'Volvo'},
-    {value: 'saab', viewValue: 'Saab'},
-    {value: 'mercedes', viewValue: 'Mercedes'}
-  ];
-  foodControl = new FormControl(this.foods[2].value);
-  carControl = new FormControl(this.cars[1].value);
+  requirementForm: FormGroup;
+  typeBulkList: string[] = ['Carga seca', 'Carga refrigerada', 'Carga húmeda', 'Carga de agregados', 'Otro'];
+  detailBulkList: string[] = ['Bolsas de cemento', 'Bolsas de rapimix', 'm2 de adoquines', 'Varillas de fierro 3/4', 'Otro'];
+  quantityList: string[] = ['Bolsa de cemento', 'Bolsas de rapimix', 'Peso total'];
 
+  typeBulkControl = new FormControl(this.typeBulkList.values);
+  detailBulkControl = new FormControl(this.detailBulkList.values); 
+  quantityControl = new FormControl(this.quantityList.values);
+  dataOriginControl = new FormControl();
+  dataDestinationControl = new FormControl();
 
   public selectedOrigin: OriginI = {id: '0', name: ''};
   public origins: OriginI[]=[];
   public destinations: DestinationI[]=[];
 
-
-  tipoCargaLista: string[] = ['Carga seca', 'Carga refrigerada', 'Carga húmeda', 'Carga de agregados', 'Otro'];
-  detalleCargaLista: string[] = ['Bolsas de cemento', 'Bolsas de rapimix', 'm2 de adoquines', 'Varillas de fierro 3/4', 'Otro'];
-  cantidadesLista: string[] = ['Bolsa de cemento', 'Bolsas de rapimix', 'Pedo total'];
-
-  tipoCarga = new FormControl(this.tipoCargaLista.values);
-
-  detalleCarga = new FormControl(this.detalleCargaLista.values); 
-
-  cantidades = new FormControl();
-  
-
-  dataOrigin = new FormControl();
-  dataDestination = new FormControl();
-
-
   constructor(
     private originService: OriginService
   ) {
-    this.form = new FormGroup({
-      food: this.foodControl,
-      car: this.carControl
+    this.requirementForm = new FormGroup({
+      typeBulk: this.typeBulkControl,
+      detailBulk: this.detailBulkControl,
+      quantity: this.quantityControl,
+      dataOtigin: this.dataOriginControl,
+      dataDestination: this.dataDestinationControl
     });
   }
-  
 
   ngOnInit(): void {
     this.origins = this.originService.getOrigin();
