0b8979973f37aebb365cae6c7a81c5ac32a23e0f
Katherine-fe <kattyserranobasan@gmail.com>
2021-07-20 11:00:57 -0500

    new truck

---
+++ src/app/models/truck.ts
@@ -0,0 +1,9 @@
+export interface Truck {
+    tarjetaPropiedad: string,
+    soat: string,
+    licenciaConducir: string,
+    nameDriver: string,
+    docIdent: string,
+    tolvaForm: string,
+    capCarga: string
+}
