7a76cf6d55a46027e791215cd203829b9f315bdd
Katherine-fe <kattyserranobasan@gmail.com>
2021-07-21 22:18:59 -0500

    get truck

--- src/app/view/main-driver/components/select-truck/select-truck.component.ts
+++ src/app/view/main-driver/components/select-truck/select-truck.component.ts
@@ -9,21 +9,31 @@ import { TruckService } from 'src/app/services/truck.service';
 })
 export class SelectTruckComponent implements OnInit {
 
+  trucks!: any;
+  idUserConectado = JSON.parse(sessionStorage.getItem('user') || '').id;
   constructor( private router: Router, private truckService: TruckService) { }
 
   ngOnInit(): void {
+    this.getTrucks(this.idUserConectado)
   }
   getTrucks(idUser: string){
     this.truckService.getTruckID(idUser).subscribe((truckSnapshot)=>{
-      
+      this.trucks = [];
       console.log(truckSnapshot);
       truckSnapshot.forEach((elem: any) =>{
         console.log(elem.payload.doc.id);
-        //console.log(elem.payload.doc.data());
-        
-        
-      })
+        console.log(elem.payload.doc.data().placa);
 
+        this.trucks.push({
+          id: elem.payload.doc.id,
+          capCarga: elem.payload.doc.data().capCarga,
+          placa: elem.payload.doc.data().placa,
+          tolvaForm: elem.payload.doc.data().tolvaForm,
+          userId: elem.payload.doc.data().userId,
+          nameDriver: elem.payload.doc.data().nameDriver
+        });
+
+      })
 
     })
   }
