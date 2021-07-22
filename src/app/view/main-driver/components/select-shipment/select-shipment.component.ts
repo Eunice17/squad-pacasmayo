8ce6f3ab7db5d488a34834f5ff9c2f8b44dd1701
Eunice Fiorella <euniceancajima@gmail.com>
2021-07-21 23:17:11 -0500

    Seleccionar ciudades

--- src/app/view/main-driver/components/select-shipment/select-shipment.component.ts
+++ src/app/view/main-driver/components/select-shipment/select-shipment.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-select-shipment',
   templateUrl: './select-shipment.component.html',
   styleUrls: ['./select-shipment.component.scss']
 })
 export class SelectShipmentComponent implements OnInit {
-  ciudad = ['All', 'Piura', 'Trujillo'];
+  ciudad = ['All', 'Chiclayo', 'Ferreñafe',
+    'Lambayaque', 'Morropón', 'Ayabaca',
+    'Huancabamba', 'Paita', 'Piura',
+    'Sullana', 'Talara', 'Sechura',
+    'Trujillo', 'Chanchan', 'Escalada',
+    'Laredo', 'Chicago', 'Salaverry',
+    'Cajabamba', 'Cajamarca', 'Calendín', 'Chota',
+    'Contumazá', 'Cutervo', 'Hualgayoc', 'Jaén', 'San Ignacio',
+    'San Miguel', 'Santa Cruz', 'San Marcos', 'San Pablo'];
+  city = new FormControl('', [Validators.required]);
   constructor() { }
 
   ngOnInit(): void {
